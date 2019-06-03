3d48d66f81a9185ebc808203af57a2eac97b2333
Przemysław Czechowski <przemyslaw.czechowski@gmail.com>
2019-06-03 17:19:40 +0200

    Implemented loading to single data frame and clearing

--- download.py
+++ download.py
@@ -3,16 +3,16 @@ import urllib.request
 import os
 RESOURCES_DIR="resources"
 RESOURCE_EXT=".csv"
-def createUrl(companyCode: str):
-    return "https://stooq.pl/q/d/l/?s={}&d1=20050101&d2=20100101&i=d".format(companyCode)
+def createUrl(company_code: str):
+    return "https://stooq.pl/q/d/l/?s={}&d1=20050101&d2=20100101&i=d".format(company_code)
 
 def processList(listFileName: str):
     os.makedirs(os.path.join(os.getcwd(), RESOURCES_DIR), exist_ok=True)
     with open(listFileName) as f:
         for line in f:
-            companyCode = line.rstrip('\n').split(" ")[2].lower()
-            resource_path = os.path.join(os.getcwd(), RESOURCES_DIR, companyCode + RESOURCE_EXT)
-            url = createUrl(companyCode)
+            company_code = line.rstrip('\n').split(" ")[2].lower()
+            resource_path = os.path.join(os.getcwd(), RESOURCES_DIR, company_code + RESOURCE_EXT)
+            url = createUrl(company_code)
             print("downloading: "+url+" to "+resource_path)
             urllib.request.urlretrieve(url, resource_path)
 
---
+++ loader.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+import os
+import pandas
+RESOURCES_DIR="resources"
+RESOURCE_EXT=".csv"
+RESOURCE_CLOSE_KEY="Zamkniecie"
+RESOURCE_DATE_KEY="Data"
+def loadDataFile(file_name: str):
+    data = pandas.read_csv(file_name)
+    data = data.rename(columns={RESOURCE_DATE_KEY:"Date", RESOURCE_CLOSE_KEY:"Close"})
+    return data[["Date", "Close"]]
+def loadAllAndClear(list_file_name: str):
+    resources_dir = os.path.join(os.getcwd(), RESOURCES_DIR)
+    with open(list_file_name) as f:
+        data = []
+        for line in f:
+            company_code = line.rstrip('\n').split(" ")[2].lower()
+            print(company_code)
+            resource_path = os.path.join(resources_dir, company_code + RESOURCE_EXT)
+            try:
+                company_data = loadDataFile(resource_path)
+            except FileNotFoundError:
+                print("File not found:" + resource_path)
+                continue
+            except KeyError:
+                print("Malformed data:" + resource_path)
+                continue
+            company_data['Symbol'] = company_code
+            data.append(company_data)
+        data = pandas.concat(data)
+        data = data.pivot('Date', 'Symbol', 'Close').reset_index()
+        print(data.head())
+        for code in data.columns[1:]:# first column is "date"
+            if data[code].isnull().iloc[0] or data[code].isnull().iloc[-1]:
+                print("Removing data for index "+code+" because only part of the time window is avaliable")
+                del data[code]
+        data = data.dropna() # remove rows with nan's
+        return data
+data = loadAllAndClear("gpw_2007_list.txt")
